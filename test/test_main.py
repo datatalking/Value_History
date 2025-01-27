ddc15b6e6d5a416bd864a9d979b7d1f9e55c9109
Andrew <paintstone@gmail.com>
2025-01-27 13:37:09 -0800

    updated PATH and test data for migration to src class

--- test/test_main.py
+++ test/test_main.py
@@ -2,6 +2,11 @@ from unittest import TestCase
 
 
 class TestRealEstateDBLoader(TestCase):
+	def test_load_data(self):
+		self.fail(None)
+
+
+class TestRealEstateDBLoader(TestCase):
 	# test that the EXPECTED_HEADERS only match EXPECTED_HEADERS for this type
 	# next test that sqlite data types are compatible with being converted to SQLserver
 	pass
