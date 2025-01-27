446ed0146a33b1cda7ec356d9ee6c48a1ff4120c
Andrew <paintstone@gmail.com>
2025-01-27 12:49:15 -0800

    Initial commit

---
+++ test/test_main.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestRealEstateDBLoader(TestCase):
+	# test that the EXPECTED_HEADERS only match EXPECTED_HEADERS for this type
+	# next test that sqlite data types are compatible with being converted to SQLserver
+	pass
